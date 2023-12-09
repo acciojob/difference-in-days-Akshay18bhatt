499a4226e87f14ea74ec80c105b6d59afb3f9ef1
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-09 12:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const d1= new Date(date1);
+	const d2= new Date(date2);
+
+	console.log(d1,d2);
+	const instance1= (d1.getTime()/1000)/(24*60*60);
+	const instance2= (d2.getTime()/1000)/(24*60*60);
+	const diff=instance2-instance1;
+	// console.log(diff);
+	return diff;
+	
+	
+	
 };
 
 // Do not change the code below.
